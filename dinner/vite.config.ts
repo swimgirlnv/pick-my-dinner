7e52f593e8ee44e694925c8f0d30501d897abeef
Rebecca Waterson <rebeccawaterson@Rebeccas-MBP.lan>
2024-05-30 19:47:11 -0700

    updates to config

--- dinner/vite.config.ts
+++ dinner/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios'] // , '@mui/material'
+      external: ['axios', '@mui/material'] // , '@mui/material'
     }
   }
 })
